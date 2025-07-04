ad3c58a5a50b4671b386719ba3751e9a89d03ccb
GrubtheBub <102131733+Randabble@users.noreply.github.com>
2025-07-03 21:46:01 -0700

    adad

--- App.tsx
+++ App.tsx
@@ -1,19 +1,20 @@
+import React from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
-import About from "@/pages/about";
-import Team from "@/pages/team";
-import Impact from "@/pages/impact";
-import Product from "@/pages/product";
-import Blog from "@/pages/blog";
-import BlogPost from "@/pages/blog-post";
-import Contact from "@/pages/contact";
-import Volunteer from "@/pages/volunteer";
-import Partner from "@/pages/partner";
-import NotFound from "@/pages/not-found";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+import Home from "./home";
+import About from "./about";
+import Team from "./team";
+import Impact from "./impact";
+import Product from "./product";
+import Blog from "./blog";
+import BlogPost from "./blog-post";
+import Contact from "./contact";
+import Volunteer from "./volunteer";
+import Partner from "./partner";
+import NotFound from "./not-found";
 
 function Router() {
   return (
--- home.tsx
+++ home.tsx
@@ -1,14 +1,15 @@
-import Navigation from "@/components/Navigation";
-import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import ImpactSection from "@/components/ImpactSection";
-import ProductSection from "@/components/ProductSection";
-import BlogSection from "@/components/BlogSection";
-import PartnershipSection from "@/components/PartnershipSection";
-import CTASection from "@/components/CTASection";
-import FAQSection from "@/components/FAQSection";
-import Footer from "@/components/Footer";
-import BackToTop from "@/components/BackToTop";
+import React from "react";
+import Navigation from "./components/Navigation";
+import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
+import ImpactSection from "./components/ImpactSection";
+import ProductSection from "./components/ProductSection";
+import BlogSection from "./components/BlogSection";
+import PartnershipSection from "./components/PartnershipSection";
+import CTASection from "./components/CTASection";
+import FAQSection from "./components/FAQSection";
+import Footer from "./components/Footer";
+import BackToTop from "./components/BackToTop";
 
 export default function Home() {
   return (
